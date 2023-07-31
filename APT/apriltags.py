715b4d42a721d6fd151d9d7e404f84cda978e915
Dyoseus <albert_h_le@hotmail.com>
2023-07-31 18:02:58 -0400

    idk, might change stuff?

--- APT/apriltags.py
+++ APT/apriltags.py
@@ -2,8 +2,60 @@ from dt_apriltags import Detector
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from pymavlink import mavutil
+import sys
+import signal
 
 from pid import PID
+
+def set_rc_channel_pwm(mav, channel_id, pwm=1500):
+    """Set RC channel pwm value
+    Args:
+        channel_id (TYPE): Channel ID
+        pwm (int, optional): Channel pwm value 1100-1900
+    """
+    if channel_id < 1 or channel_id > 18:
+        print("Channel does not exist.")
+        return
+
+    # Mavlink 2 supports up to 18 channels:
+    # https://mavlink.io/en/messages/common.html#RC_CHANNELS_OVERRIDE
+    rc_channel_values = [65535 for _ in range(18)]
+    rc_channel_values[channel_id - 1] = pwm
+    mav.mav.rc_channels_override_send(
+        mav.target_system,  # target_system
+        mav.target_component,  # target_component
+        *rc_channel_values
+    )
+
+
+def set_vertical_power(mav, power=0):
+    """Set vertical power
+    Args:
+        power (int, optional): Vertical power value -100-100
+    """
+    if power < -100 or power > 100:
+        print("Power value out of range. Clipping...")
+        power = np.clip(power, -100, 100)
+
+    power = int(power)
+
+    set_rc_channel_pwm(mav, 3, 1500 + power * 5)
+
+
+def press_to_depth(pressure):
+    """Convert pressure to depth
+    Args:
+        pressure (float): Pressure in hPa
+    Returns:
+        float: Depth in water in meters
+    """
+    rho = 1029  # density of fresh water in kg/m^3
+    g = 9.81  # gravity in m/s^2
+    pressure_at_sea_level = 1013.25  # pressure at sea level in hPa
+    # multiply by 100 to convert hPa to Pa
+    return (pressure - pressure_at_sea_level) * 100 / (rho * g)
+
 
 def readframes(video):
     video = cv2.VideoCapture(f'{video}')
@@ -48,15 +100,33 @@ def size(video):
 
     return middle_y, middle_x
 
-print(size('AprilTagTest.mkv'))
 
-def changePos(video, l):
+def main():
+
+    mav = mavutil.mavlink_connection("udpin:0.0.0.0:14550")
+    pid = PID(45, 0.0, 10.0, 2)
+    video = 'AprilTagTest.mkv'
     middle_y, middle_x = size(video)
-    for i in range(len(l)):
-        if middle_y > l[0]:
-            print('needs to go up')
-        else:
-            print('needs to go down')
+    readframes(video)
 
     
-aptags = readframes()
+
+    while True:
+        # Read the current coordinates from the AprilTag detector
+        current_y, current_x = readframes(video)
+
+        # Calculate error from the desired middle coordinates
+        error_y = middle_y - current_y
+        error_x = middle_x - current_x
+
+        # Update the PID controllers and get the output
+        output_y = pid.update(error_y)
+        output_x = pid.update(error_x)
+
+        # Set vertical power using the PID output
+        set_vertical_power(mav, -output_y)  # Negative because of the direction of the thruster
+
+        # Set horizontal power using the PID output
+        set_rc_channel_pwm(mav, 6, pwm=1500 + output_x)
+
+    
